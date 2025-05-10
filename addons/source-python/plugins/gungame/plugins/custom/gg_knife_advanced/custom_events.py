cb6535634ae2cea4ec6e4a4a4368139096a6c779
satoon101 <satoon101@gmail.com>
2025-05-09 20:04:37 -0400

    Updates for Ruff.
    Moved all swap functionality into 1 delay.

--- addons/source-python/plugins/gungame/plugins/custom/gg_knife_advanced/custom_events.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_knife_advanced/custom_events.py
@@ -15,30 +15,29 @@ from gungame.core.events.resource import GGResourceFile
 # Plugin
 from .info import info
 
-
 # =============================================================================
 # >> ALL DECLARATION
 # =============================================================================
 __all__ = (
-    'GG_Level_Swapped',
+    "GG_Level_Swapped",
 )
 
 
 # =============================================================================
 # >> CLASSES
 # =============================================================================
-# pylint: disable=too-few-public-methods
+# ruff: noqa: N801
 class GG_Level_Swapped(CustomEvent):
     """Called when a player steals a level by knifing."""
 
     attacker = leveler = ShortVariable(
-        'The userid of the player that stole the level'
+        "The userid of the player that stole the level",
     )
-    attacker_level = ByteVariable('The new level of the attacker')
+    attacker_level = ByteVariable("The new level of the attacker")
     userid = victim = ShortVariable(
-        'The userid of the player that had a level stolen'
+        "The userid of the player that had a level stolen",
     )
-    victim_level = ByteVariable('The new level of the victim')
+    victim_level = ByteVariable("The new level of the victim")
 
 
 # =============================================================================
--- addons/source-python/plugins/gungame/plugins/custom/gg_knife_advanced/gg_knife_advanced.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_knife_advanced/gg_knife_advanced.py
@@ -19,78 +19,54 @@ from .custom_events import GG_Level_Swapped
 # =============================================================================
 # >> GUNGAME PRE-EVENTS
 # =============================================================================
-@PreEvent('gg_knife_steal')
+@PreEvent("gg_knife_steal")
 def _pre_knife_steal(game_event):
-    previous_attacker_level = game_event['attacker_level'] - 1
-    previous_victim_level = game_event['victim_level'] + 1
+    previous_attacker_level = game_event["attacker_level"] - 1
+    previous_victim_level = game_event["victim_level"] + 1
     difference = previous_victim_level - previous_attacker_level
-
     if difference <= 1:
         return EventAction.CONTINUE
 
-    levels = abs(difference) - 1
-    killer = player_dictionary[game_event['attacker']]
-    victim = player_dictionary[game_event['victim']]
-
     Delay(
         delay=0,
-        callback=killer.increase_level,
+        callback=_swap_player_levels,
         kwargs={
-            'levels': levels,
-            'reason': 'swap',
-            'victim': victim.userid,
-            'sound_name': 'swapped_up',
-        },
-    )
-    Delay(
-        delay=0,
-        callback=victim.decrease_level,
-        kwargs={
-            'levels': levels,
-            'reason': 'swap',
-            'attacker': killer.userid,
-            'sound_name': 'swapped_down',
-        }
-    )
-    Delay(
-        delay=0,
-        callback=_fire_swap_event,
-        kwargs={
-            'killer': killer.userid,
-            'killer_level': previous_victim_level,
-            'victim': victim.userid,
-            'victim_level': previous_attacker_level,
+            "attacker": game_event["attacker"],
+            "attacker_level": previous_victim_level,
+            "victim": game_event["victim"],
+            "victim_level": previous_attacker_level,
+            "levels": abs(difference) - 1,
         }
     )
     return EventAction.CONTINUE
 
 
-@PreEvent('gg_level_up')
+@PreEvent("gg_level_up")
 def _pre_level_up(game_event):
-    if game_event['reason'] != 'steal':
+    if game_event["reason"] != "steal":
         return EventAction.CONTINUE
 
-    level = game_event['new_level']
-    if level != game_event['old_level'] + 1:
+    level = game_event["new_level"]
+    if level != game_event["old_level"] + 1:
         return EventAction.CONTINUE
 
-    victim = player_dictionary[game_event['victim']]
+    victim = player_dictionary[game_event["victim"]]
     if victim.level <= level:
         return EventAction.CONTINUE
 
     return EventAction.BLOCK
 
 
-@PreEvent('gg_level_down')
+@PreEvent("gg_level_down")
 def _pre_level_down(game_event):
-    if game_event['reason'] != 'steal':
+    if game_event["reason"] != "steal":
         return EventAction.CONTINUE
 
-    level = game_event['new_level']
-    if level != game_event['old_level'] - 1:
+    level = game_event["new_level"]
+    if level != game_event["old_level"] - 1:
         return EventAction.CONTINUE
 
-    killer = player_dictionary[game_event['attacker']]
+    killer = player_dictionary[game_event["attacker"]]
     if killer.level >= level:
         return EventAction.CONTINUE
 
@@ -100,9 +76,23 @@ def _pre_level_down(game_event):
 # =============================================================================
 # >> HELPER FUNCTIONS
 # =============================================================================
-def _fire_swap_event(killer, killer_level, victim, victim_level):
+def _swap_player_levels(attacker, attacker_level, victim, victim_level, levels):
+    killer = player_dictionary[attacker]
+    victim = player_dictionary[victim]
+    killer.increase_level(
+        levels=levels,
+        reason="swap",
+        victim=victim.userid,
+        sound_name="swapped_up",
+    )
+    victim.decrease_level(
+        levels=levels,
+        reason="swap",
+        attacker=attacker,
+        sound_name="swapped_down",
+    )
     with GG_Level_Swapped() as event:
-        event.attacker = event.leveler = killer
-        event.attacker_level = killer_level
-        event.userid = event.victim = victim
+        event.attacker = event.leveler = attacker
+        event.attacker_level = attacker_level
+        event.userid = event.victim = victim.userid
         event.victim_level = victim_level
